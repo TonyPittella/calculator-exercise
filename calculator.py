8b1dc3b9618e40bdd274b1f162bb64730eb43949
TreySchneider95 <tschneid95@gmail.com>
2021-09-09 07:44:48 -0700

    Added remainder function and remainder feature tested and works

--- calculator.py
+++ calculator.py
@@ -4,13 +4,20 @@ import argparse
 def add_func(a,b):
 	return a + b
 
+def remainder_func(a,b):
+	return a % b
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
 	#add arguments here
-	parser.add_argument('--add',mnargs=2, type=int, help='adds two numbers')
+	parser.add_argument('--add',nargs=2, type=int, help='adds two numbers')
+	parser.add_argument('--remainder',nargs=2, type=int, help='adds two numbers')
 	args = parser.parse_args()
 
 	if args.add:
 		print( add_func(args.add[0], args.add[1]))
 	print(args)
+
+	if args.remainder:
+		print(remainder_func(args.remainder[0], args.remainder[1]))
