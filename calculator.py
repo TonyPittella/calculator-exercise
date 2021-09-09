8388a66fa61af931cda69b210bae4a3b79bfdf62
TonyPittella <atpittella@gmail.com>
2021-09-09 10:53:50 -0400

    added multiplication functionality to calculator

--- calculator.py
+++ calculator.py
@@ -4,13 +4,23 @@ import argparse
 def add_func(a,b):
 	return a + b
 
+def multiply_func(a,b):
+    return a * b
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
 	#add arguments here
-	parser.add_argument('--add',mnargs=2, type=int, help='adds two numbers')
+	parser.add_argument('--add',nargs=2, type=int, help='adds two numbers')
+	parser.add_argument('--multiply', nargs=2, type=int, help='multiply two numbers')
 	args = parser.parse_args()
 
 	if args.add:
 		print( add_func(args.add[0], args.add[1]))
 	print(args)
+
+
+	if args.multiply:
+    		print(multiply_func(args.add[0],args[1]))
+	print(args)			
+	
