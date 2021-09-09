b6828ff550d047353866ba1e2d768e7250073182
Shaquell Nurse <shaquellnurse@Shaquells-MacBook-Pro.local>
2021-09-09 11:18:56 -0400

    i added subtraction branch

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@ import argparse
 
 
 def add_func(a,b):
-	return a + b
+	return a - b
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
 	#add arguments here
-	parser.add_argument('--add',mnargs=2, type=int, help='adds two numbers')
+	parser.add_argument('--sub',mnargs=2, type=int, help='subtracts two numbers')
 	args = parser.parse_args()
 
-	if args.add:
-		print( add_func(args.add[0], args.add[1]))
+	if args.sub:
+		print( add_func(args.sub[0], args.sub[1]))
 	print(args)
